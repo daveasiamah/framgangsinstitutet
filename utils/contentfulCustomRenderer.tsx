f6795cc31c904fe30dafb4f5569a67b606417770
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2024-02-10 14:58:04 +0000

    updates

--- utils/contentfulCustomRenderer.tsx
+++ utils/contentfulCustomRenderer.tsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS } from '@contentful/rich-text-types';
+import { BLOCKS, TopLevelBlock } from '@contentful/rich-text-types';
 import Image from 'next/image';
 
-interface Node {
-  nodeType: string;
-  content?: Node[];
-  data?: {
-    target?: {
-      fields?: {
-        file?: {
-          url: string;
-          title?: string;
-          details?: {
-            image?: {
-              width: number;
-              height: number;
-            };
-          };
-        };
-      };
-    };
-  };
-}
+// interface Node {
+//   nodeType: string;
+//   content?: Node[];
+//   data?: {
+//     target?: {
+//       fields?: {
+//         file?: {
+//           url: string;
+//           title?: string;
+//           details?: {
+//             image?: {
+//               width: number;
+//               height: number;
+//             };
+//           };
+//         };
+//       };
+//     };
+//   };
+// }
 
-const ContentfulRichText: React.FC<{ content: Node[] }> = ({ content }) => {
+const ContentfulRichText: React.FC<{ content: TopLevelBlock[] }> = ({ content }) => {
   return (
     <div>
-      {documentToReactComponents({ data: {}, content }, {
+      {documentToReactComponents({
+        data: {}, content,
+        nodeType: BLOCKS.DOCUMENT
+      }, {
           preserveWhitespace: true,
           renderNode: {
             [BLOCKS.EMBEDDED_ASSET]: (node) => {
