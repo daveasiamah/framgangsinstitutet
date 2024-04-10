292d2f3a0d938a6e16cee9d343712ea59312b2a4
KhaleequeurRehman <kmem98@gmail.com>
2024-04-10 10:49:46 +0500

    added popup on start now button

--- components/parts/ButtonArrow.tsx
+++ components/parts/ButtonArrow.tsx
@@ -5,7 +5,9 @@ import { MdKeyboardArrowRight } from "react-icons/md"
 type Props = {
   className?: string
   isReverse?: boolean
-  href: string
+  href: string,
+  as: string,
+  onClick: () => void,
   children: React.ReactNode
 }
  
@@ -13,9 +15,33 @@ export default function ButtonArrow({
   className,
   isReverse,
   children,
-  href
+  href,
+  as="link",
+  onClick
 }: Props) {
   return (
+
+    as === "btn" ? 
+    (
+      <button
+      className={`btn btn-outline ${
+        isReverse ? "btn-icon-reverse" : "btn-icon"
+      }  border-2 ${
+        isReverse ? "border-white text-white" : "border-primary text-primary "
+      } pr-2 pl-4 ${className}`}
+      onClick={onClick}
+    >
+      {children}
+      <MdKeyboardArrowRight
+        className={`${
+          isReverse ? "bg-white/40 text-white" : "bg-primary text-white"
+        }  p-1 rounded-lg ml-2`}
+        size={34}
+      />
+    </button>
+    ) 
+    :
+    (
     <Link
       href={href}
       className={`btn btn-outline ${
@@ -32,5 +58,7 @@ export default function ButtonArrow({
         size={34}
       />
     </Link>
+    )
+    
   )
 }
