12a4006f4b27783f9f5358c8d1727191a451a999
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2024-06-13 07:15:02 +0000

    updates

--- pages/404.tsx
+++ pages/404.tsx
@@ -11,14 +11,13 @@ return(
         <div className="flex flex-col items-center justify-center md:py-24 lg:py-32">
           <h1 className="font-bold text-blue-600 text-9xl">404</h1>
           <p className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-            <span className="text-red-500">Oops!</span> Page{" "}
             
           </p>
           <p className="mb-8 text-center text-gray-500 md:text-lg">
-            The page you’re looking for doesn’t exist.
+            Sidan du letar efter finns inte.
           </p>
                     <Link href="/" className="px-5 py-2 rounded-md text-blue-100 bg-blue-600 hover:bg-blue-700">
-                        Back to Home
+                        Till Hemsidan
                     </Link>
         </div>
         
