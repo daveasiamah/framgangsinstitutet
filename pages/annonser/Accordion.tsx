ba58404c8343e3ea86f0f1dc1824273c5e241470
Michael Yeboah <mbaah80@gmail.com>
2024-09-19 23:48:08 +0000

    annonser

---
+++ pages/annonser/Accordion.tsx
@@ -0,0 +1,56 @@
+import React, { useRef, useState } from "react"
+
+type Props = {
+  data: {
+    id: number
+    question: string
+    answer: string
+  }[]
+}
+
+function Accordion({ data }: Props) {
+  const [selected, setSelected] = useState<number | null>(null)
+
+  const ref = useRef<HTMLInputElement>(null)
+
+  const toggle = (index: number) => {
+    if (selected === index) {
+      return setSelected(null)
+    }
+
+    setSelected(index)
+  }
+
+  return (
+    <div className="grid gap-6">
+      {data.map((data, index) => (
+        <button
+          onClick={() => toggle(index)}
+          key={data.id}
+          className="accordion-wrapper faq-item_ann text-left bg-white border-none border-gray-200 hover:border-primary py-4 px-6 rounded-xl cursor-pointer focus:border-primary"
+        >
+          <div className="cursor-pointer py-2 flex justify-between items-center">
+            <div className="font-semibold text-lg text-subtitle-dark">
+              {data.question}
+            </div>
+          </div>
+          <div
+            ref={ref}
+            style={{
+              height: selected === index ? "auto" : 0
+            }}
+            className={`text-subtitle pr-4 ${
+              selected === index
+                ? "accordion-content show bounce-pricing"
+                : "accordion-content"
+            }`}
+          >
+            {data.answer}
+          </div>
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default Accordion
