0a0f4f707bf528d55b5cc9ab7dd688ace54e5f7f
David Asiamah <daveasiamah@gmail.com>
2025-01-04 20:23:35 +0000

    fix footer responsivenes

--- components/parts/Footer.tsx
+++ components/parts/Footer.tsx
@@ -17,77 +17,56 @@ export default function Footer({}: Props) {
 
   return (
     <footer className="bg-[#fff] text-[#434C69] py-14 relative mt-12 lg:mt-10">
-      <div className="container mx-auto border-2">
-        <div className="flex flex-col lg:flex-row justify-between items-center text-left md:text-center lg:text-left lg:items-end lg:gap-[120px]">
-          <div className="w-full h-full flex flex-col lg:w-1/3 items-start md:items-center lg:self-start">
-            <Image
-              className="mb-2 md:w-[153px] md:h-full"
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row lg:justify-between lg:items-start gap-10">
+          {/* Logo Section */}
+          <div className="w-full lg:w-1/3 flex flex-col items-start text-left">
+            <img
               src="/checkified-logo-main.svg"
               alt="checkified"
-              height={51}
-              width={153}
-              priority
+              className="h-[48px] w-[213px] object-left object-contain"
             />
-            <div>
-              <p className="text-[#434C69] text-[14px] leading-[22px] md:text-[16px] font-[600] font-inter md:leading-[32px]">
-                Börja Med Dropshipping Sverige
-              </p>
-            </div>
+            <p className="text-[14px] md:text-[16px] font-semibold leading-[22px] md:leading-[32px]">
+              Börja Med Dropshipping Sverige
+            </p>
 
-            {/* Partner Logos Section */}
-            <div className="flex flex-col">
+            {/* Partner Logos */}
+            <div className="flex flex-col flex-wrap gap-2 mt-6">
               <img
-                className="md:h-[51px] md:w-[167px] w-[155px] h-[51px]"
                 src="/shopifypartner.svg"
                 alt="Shopify Partner"
+                className="h-[27px] w-[166px] md:w-[167px]"
               />
               <img
-                className="md:h-[51px] md:w-[167px] w-[155px] h-[51px]"
                 src="/stripe.svg"
                 alt="Stripe Partner"
+                className="h-[34px] w-[166px] md:w-[167px]"
               />
               <img
-                className="md:h-[51px] md:w-[167px] w-[155px] h-[51px]"
                 src="/google.svg"
                 alt="Google Partner"
+                className="h-[44px] w-[166px] md:w-[167px]"
               />
             </div>
           </div>
 
           {/* Links Section */}
-          <div className="w-full lg:w-1/2 grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-[90px] footer-links mt-10 lg:mt-0">
+          <div className="w-full lg:w-2/3 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-10">
+            {/* Produkter */}
             <div>
-              <h5 className="font-bold text-[22px] leading-[22px] md:leading-[32px] mb-[10px] md:mb-[20px] font-jakarta">
-                Produkter
-              </h5>
-              <ul>
+              <h5 className="font-bold font-jakarta text-xl mb-4">Produkter</h5>
+              <ul className="space-y-2 flex flex-col gap-3">
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/skool"
-                  >
-                    Dropshipping Kurs
-                  </Link>
+                  <Link href="/skool">Dropshipping Kurs</Link>
+                </li>
+                <li>
+                  <Link href="/butiker">Färdig E-Butik</Link>
+                </li>
+                <li>
+                  <Link href="/annonser">Dropshipping Ads</Link>
                 </li>
                 <li>
                   <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/butiker"
-                  >
-                    Färdig E-Butik
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/annonser"
-                  >
-                    Dropshipping Ads
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
                     href="https://whop.com/dropshipping-mentorskap"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -98,110 +77,82 @@ export default function Footer({}: Props) {
               </ul>
             </div>
 
-            {/* Företag Section */}
+            {/* Företag */}
             <div>
-              <h5 className="font-bold text-[22px] leading-[22px] md:leading-[32px] mb-[10px] md:mb-[20px] font-jakarta">
-                Företag
-              </h5>
-              <ul>
+              <h5 className="font-bold font-jakarta text-lg mb-4">Företag</h5>
+              <ul className="space-y-2 flex flex-col gap-3">
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/about"
-                  >
-                    Om oss
-                  </Link>
+                  <Link href="/about">Om oss</Link>
                 </li>
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/butiker"
-                  >
-                    Affiliate
-                  </Link>
+                  <Link href="/butiker">Affiliate</Link>
                 </li>
               </ul>
             </div>
 
-            {/* Resurser Section */}
+            {/* Resurser */}
             <div>
-              <h5 className="font-bold text-[22px] leading-[22px] md:leading-[32px] mb-[10px] md:mb-[20px] font-jakarta">
-                Resurser
-              </h5>
-              <ul>
+              <h5 className="font-bold font-jakarta text-lg mb-4">Resurser</h5>
+              <ul className="space-y-2 flex flex-col gap-3">
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/blog"
-                  >
-                    Blogg
-                  </Link>
+                  <Link href="/blog">Blogg</Link>
                 </li>
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/contact-us"
-                  >
-                    Kontakt
-                  </Link>
+                  <Link href="/contact-us">Kontakt</Link>
                 </li>
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/university"
-                  >
-                    University
-                  </Link>
+                  <Link href="/university">University</Link>
                 </li>
                 <li>
-                  <Link
-                    className="hover:underline underline-offset-4 text-xs md:text-[16px] font-[500] font-inter"
-                    href="/faq"
-                  >
-                    FAQ
-                  </Link>
+                  <Link href="/faq">FAQ</Link>
                 </li>
               </ul>
             </div>
           </div>
         </div>
 
-        <hr className="my-12 bg-slate-600" />
-        <div className="flex flex-col lg:flex-row text-center lg:text-left justify-between items-center">
-          <p className="mb-4 lg:mb-0 text-[12px] md:text-[14px] font-[500] font-inter order-3 md:order-1">
-            Copyright © {new Date().getFullYear()} Checkified. All rights
-            reserved.
+        <hr className="my-10 stroke-slate-400" />
+
+        {/* Bottom Section */}
+        <div className="flex flex-col md:flex-row justify-between items-center gap-6 text-center md:text-left">
+          {/* Copyright Section */}
+          <p className="text-sm md:order-1 order-1">
+            Copyright © {currentYear} Checkified. All rights reserved.
           </p>
-          <div className="flex justify-center flex-wrap lg:justify-start items-center gap-5 order-0 md:order-2">
+
+          {/* Social Media Icons */}
+          <div className="flex gap-4 justify-center md:order-2 order-2">
             {socialMedia(24).map((data) => (
               <Link key={data.id} href={data.url} target="_blank">
-                <div className="rounded-lg hover:bg-white hover:text-primary text-[#225AEA]">
+                <div className="text-[#225AEA] hover:text-primary">
                   {data.logo}
                 </div>
               </Link>
             ))}
           </div>
-          <ul className="flex flex-wrap items-center justify-center gap-5 order-1 md:order-3">
+
+          {/* Links Section */}
+          <ul className="flex flex-wrap justify-center gap-4 md:order-3 order-3">
             <li>
               <Link
-                className="hover:underline underline-offset-4 text-[12px] md:text-[14px] font-[500] font-inter"
                 href="/cookie-policy"
+                className="hover:underline underline-offset-4 text-sm font-medium"
               >
                 Cookiepolicy
               </Link>
             </li>
             <li>
               <Link
-                className="hover:underline underline-offset-4 text-[12px] md:text-[14px] font-[500] font-inter"
                 href="/terms-of-service"
+                className="hover:underline underline-offset-4 text-sm font-medium"
               >
                 Allmänna villkor
               </Link>
             </li>
             <li>
               <Link
-                className="hover:underline underline-offset-4 text-[12px] md:text-[14px] font-[500] font-inter"
                 href="/privacy-policy"
+                className="hover:underline underline-offset-4 text-sm font-medium"
               >
                 Integritetspolicy
               </Link>
